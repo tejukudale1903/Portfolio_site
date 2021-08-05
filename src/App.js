ae430b02db8763917238741369c205ba81e96520
tejashree kudale <tejukudale1903@gmail.com>
2021-08-05 16:18:18 +0530

    Added media query

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import MainPage from "./Components/MainPage/MainPage";
 import Contact from "./Components/Contact/Contact";
+// import { useMediaQuery } from 'react-responsive';
 
 function App() {
   return (
