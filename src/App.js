00cfba34d70bf57f1a63b843e7db4cbb5bd7f5f6
tejashree kudale <tejukudale1903@gmail.com>
2021-08-05 16:39:51 +0530

    removed margin left

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import MainPage from "./Components/MainPage/MainPage";
 import Contact from "./Components/Contact/Contact";
-// import { useMediaQuery } from 'react-responsive';
+import { useMediaQuery } from 'react-responsive';
+
+
+const Example = () => {
+  const isDesktopOrLaptop = useMediaQuery({ minWidth: 1224 })
+  const isBigScreen = useMediaQuery({ minWidth: 1824 })
+  const isTabletOrMobile = useMediaQuery({ maxWidth: 1224 })
+  const isPortrait = useMediaQuery({ orientation: 'portrait' })
+  const isRetina = useMediaQuery({ minResolution: '2dppx' })
+
 
 function App() {
   return (
